8405d05b1a1957ca82472bb6c1dab12674b1a110
yousufrahman <ryousuf766@gmail.com>
2026-01-30 11:45:35 +0600

    added nested

---
+++ js-object/nested.js
@@ -0,0 +1,22 @@
+const du ={
+    faculty : ["science" , "business", "arts" , "social science"],
+    department :{
+        science : ["cse", "eee", 'phy', 'chemistry'],
+        art : ['bangla', 'english', 'wrc' , 'ihc'],
+        business : ['accounting', 'finance', 'ib'],
+        social :{
+            mcj : "media",
+            sociology : "research"
+        }
+    }
+}
+console.log(du.department.business);
+console.log (du.department.social.sociology);
+du.department.business.push("marketing");
+console.log(du.department.business);
+console.log(du.department.social);
+du.department.social.mcj = "media and tv";
+console.log(du.department.social);
+
+du.faculty[2] = "humanities";
+console.log(du.faculty);
