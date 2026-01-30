37043662b86d07fb7627991658e36c7757783a56
yousufrahman <ryousuf766@gmail.com>
2026-01-30 17:49:16 +0600

    added lop file

---
+++ js-object/obj-loop.js
@@ -0,0 +1,12 @@
+const mobile = {
+    brand : "samsung",
+    model : "S 25 Ultra",
+    camera: "12mp",
+    price : "25000 bdt",
+    
+
+ 
+}
+for(let key in mobile){
+    console.log(key , ":" , mobile[key]);
+}
