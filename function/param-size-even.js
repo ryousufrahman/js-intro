f44a4575344010199f9ada7f63007de90816d775
yousufrahman <ryousuf766@gmail.com>
2026-01-31 23:13:56 +0600

    saw if string lenght is odd or even

---
+++ function/param-size-even.js
@@ -0,0 +1,21 @@
+function sentence(str) {
+   
+    const size= str.length;
+
+     
+   if(size%2===0){
+     console.log(false);
+    }
+   else{
+    console.log(false);
+   }
+
+
+     console.log(str, size);
+    
+}
+
+sentence('dhaka');
+
+
+
