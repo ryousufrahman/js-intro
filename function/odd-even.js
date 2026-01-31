490e668e440efe976553d9cf917d60fc6c78e979
yousufrahman <ryousuf766@gmail.com>
2026-01-31 22:50:30 +0600

    added odd and even method to function

---
+++ function/odd-even.js
@@ -0,0 +1,20 @@
+function odd(x) {
+    if(x%2===1){
+        return true;
+    }
+    return false;
+
+    
+}
+
+console.log(odd(30));
+
+function even(y) {
+    if(y%2===0){
+        return true;
+    }
+    return false;
+
+    
+}
+console.log(even(30));
---
+++ function/return.js
@@ -0,0 +1,13 @@
+// we can store the function output/result to a varriable and we can do what we want to do with that varriable later
+
+function tenTimes(num1) {
+    const result= num1*10;
+    return result;
+    
+}
+
+const tenTimesResult = tenTimes(55);
+console.log(tenTimesResult);
+
+const tenTimesResult2 =tenTimes(88);
+console.log(tenTimesResult2);
